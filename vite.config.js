0b6fc95fff369a4eff71baa3ea3302ed08d4f42f
Cristobal <FHCristobal@hotmail.com>
2025-03-23 14:26:57 -0300

    base a vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/heroes-app-react/', // <-- Aquí va el nombre de tu repo
   build: {
     outDir: 'docs', // <--- Aquí cambias "dist" por "docs"
   },
